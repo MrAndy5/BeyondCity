123fec7f1a637c68837d20e8a1a961d9b7f9281d
MrAndy5 <andres.sanchezdeag@gmail.com>
2025-04-24 17:38:44 +0200

    Inicializacion

---
+++ src/main/java/com/BC/BeyondCity/BeyondCityApplication.java
@@ -0,0 +1,13 @@
+package com.BC.BeyondCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BC/BeyondCity/BeyondCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BC.BeyondCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
